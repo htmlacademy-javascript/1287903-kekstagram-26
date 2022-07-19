48fea0ec32cd59a0f78b13a3a1971c3cd4b6cde4
sarvanshiriev <sarvan.shiriev@mail.ru>
2022-07-19 16:53:03 +0500

    Правда или действие (часть 2)

--- js/fullsize.js
+++ js/fullsize.js
@@ -3,12 +3,24 @@ import { randomDescriptions } from './data.js';
 
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureImg = document.querySelector('.big-picture__img');
-const socialCommentCount = document.querySelector('.social__comment-count');
 const commentsLoader = document.querySelector('.comments-loader');
-const body = document.querySelector('.body');
+const body = document.querySelector('body');
 const bigPictureCancel = document.querySelector('.big-picture__cancel');
 const socialComments = document.querySelector('.social__comments');
 const commmmentsFragment = document.createDocumentFragment();
+let COMMENT_AMOUNT = 5;
+//  Загрузить ещё комментарии
+const commentsLoaderFunction = () => {
+  for (let i=COMMENT_AMOUNT;i<=COMMENT_AMOUNT+5;i++) {
+    if (socialComments.children[i]){
+      socialComments.children[i].classList.remove('hidden');
+      bigPicture.querySelector('.social__comment-count').textContent=`${i} из ${socialComments.children.length} комментариев`;
+    }    }
+  COMMENT_AMOUNT+=5;
+  if ((socialComments.children.length-COMMENT_AMOUNT) < 5) {
+    commentsLoader.classList.add('hidden');
+  }
+};
 
 // Список комментариев под фотографией:
 const renderComments = function (pictureId) {
@@ -38,37 +50,37 @@ const renderComments = function (pictureId) {
     bigPicture.querySelector('.social__comment-count').textContent=`${result.length} из ${commentsArray.length} комментариев`;
   });
   socialComments.appendChild(commmmentsFragment);
+  commentsLoader.addEventListener('click',commentsLoaderFunction);
 };
 
 // Вешаем обработчик на сетку
 pictureElement.addEventListener('click' ,  (evt) => {
   if (evt.target.nodeName === 'A'|| evt.target.closest('a'))
   { bigPicture.classList.remove('hidden');
-  const eventTarget = evt.target.closest('a');
-  const pictureDataAtribute = Number(eventTarget.dataset.pictureId);
-
-
-  bigPictureImg.querySelector('img').src=eventTarget.querySelector('.picture__img').src;
+    const eventTarget = evt.target.closest('a');
+    const pictureDataAtribute = Number(eventTarget.dataset.pictureId);
+    bigPictureImg.querySelector('img').src=eventTarget.querySelector('.picture__img').src;
     bigPicture.querySelector('.big-picture__img img').alt=eventTarget.querySelector('.picture__img').alt;
     bigPicture.querySelector('.likes-count').textContent=eventTarget.querySelector('.picture__likes').textContent;
     bigPicture.querySelector('.social__caption').textContent=eventTarget.querySelector('.picture__img').alt;
     renderComments(pictureDataAtribute);
   }
+
   // Фиксируем контейнер с фотографиями
   body.classList.add('modal-open');
 });
-
-
 
 // Код для закрытия окна по нажатию клавиши Esc и клике по иконке закрытия.
 bigPictureCancel.addEventListener('click', () => {
   bigPicture.classList.add('hidden');
   body.classList.remove('modal-open');
+  commentsLoader.removeEventListener('click',commentsLoaderFunction);
 });
 document.addEventListener('keydown' ,  (evt) =>{
   if (evt.code === 'Escape' ) {
     bigPicture.classList.add('hidden');
     body.classList.remove('modal-open');
+    commentsLoader.removeEventListener('click',commentsLoaderFunction);
   }
 });
 export {body};
